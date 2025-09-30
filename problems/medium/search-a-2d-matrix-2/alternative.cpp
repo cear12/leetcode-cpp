c7aab50d202daa57af66e87085251331a8d613ec
Ole <ole@MacBook-Air.local>
2025-09-30 22:21:53 +0700

    add solutions for LeetCode - Search A 2D Matrix II

---
+++ problems/medium/search-a-2d-matrix-2/alternative.cpp
@@ -0,0 +1,16 @@
+// O(m log n) time, O(1) space
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (const auto& row : matrix) {
+            int l = 0, r = row.size() - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (row[mid] == target) return true;
+                else if (row[mid] < target) l = mid + 1;
+                else r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
